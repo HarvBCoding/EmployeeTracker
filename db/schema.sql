ad6d0699393bfd793b6bffbd369ce647d95ad655
Breania Harvey <harvey.breania@gmail.com>
2021-10-26 23:33:00 -0500

    starter code and root directory set up

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    departmentId INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (departmentId) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roleId INTEGER,
+    managerId INTEGER
+    CONSTRAINT fk_role FOREIGN KEY (roleId) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_employee FOREIGN KEY (managerId) REFERENCES employee(id) ON DELETE CASCADE
+);
---
+++ server.js
@@ -0,0 +1,5 @@
+// requires (including console.table)
+// require functions from folder
+// start inquirer 
+// start w/ list with all applicable choices
+// prompt with cases for each choice
